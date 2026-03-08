b568e2bd9ddb2e68126196e939bba642c3b24e6b
eduardoflorenciano <eduardoflorencianoo@gmail.com>
2026-03-08 20:50:59 -0300

    Adição das classes que farão o sistema funcionar

---
+++ src/main/java/model/Usuario.java
@@ -0,0 +1,7 @@
+package model;
+
+public class Usuario {
+    public static void main(String[] args) {
+        System.out.println("Teste");
+    }
+}
---
+++ src/main/java/service/ServicoService.java
@@ -0,0 +1,7 @@
+package service;
+
+public class ServicoService {
+    public static void main(String[] args) {
+        System.out.println("Teste");
+    }
+}
