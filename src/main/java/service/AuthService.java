0685bf814e25c70c7a2c0f2bf63a5818300787dd
vittor <vittor.moreno@escola.pr.gov.br>
2026-03-08 22:27:07 -0300

    codigo para cadastrar o usuario e verificando e-mail e senha

--- src/main/java/service/AuthService.java
+++ src/main/java/service/AuthService.java
@@ -12,7 +12,24 @@ public class AuthService {
     public AuthService() {
         usuarios.add(new Admin("Administrador", "admin@oficina.com", "admin123"));
     }
+    // Cadastra um novo usuário se o email aina não existir
+    public void cadastrarUsuario(String nome, String email, String senha) {
+        if (buscarPorEmail(email) != null) {
+            System.out.println("\u001B[33mEste email já está cadastrado\u001B[0m");
+            return;
+        }
+        usuarios.add(new Admin(nome, email, senha));
+        System.out.println("\u001B[32mUsuário cadastrado com sucesso!\u001B[0m");
+    }
 
+    // Verifica email e senha, retorna o Usuario se corretos, ou null se errados
+    public Usuario login(String email, String senha) {
+        Usuario usuario = buscarPorEmail(email);
+        if (usuario != null && usuario.getSenha().equals(senha)) {
+            return usuario;
+        }
+        return null;
+    }
 
 
 }
