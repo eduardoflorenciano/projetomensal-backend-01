303bcf7323c71c531426e29bded98fea5e04ef3a
vittor <vittor.moreno@escola.pr.gov.br>
2026-03-08 22:02:47 -0300

    implemente cadastro de usuario

--- src/main/java/service/AuthService.java
+++ src/main/java/service/AuthService.java
@@ -1,7 +1,26 @@
 package service;
+import model.Admin;
+import model.Usuario;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AuthService {
-    public static void main(String[] args) {
-        System.out.println("Teste");
+    // Lista de usuários cadastrados no sistema
+    private List<Usuario> usuarios = new ArrayList<>();
+
+    // Adiciona um usuário padrão ao iniciar o sistema
+    public AuthService() {
+        usuarios.add(new Admin("Administrador", "admin@oficina.com", "admin123"));
     }
+
+    // Cadastra um novo usuário se o email aina não existir
+    public void cadastrarUsuario(String nome, String email, String senha) {
+        if (buscarPorEmail(email) != null) {
+            System.out.println("\u001B[33mEste email já está cadastrado\u001B[0m");
+            return;
+        }
+        usuarios.add(new Admin(nome, email, senha));
+        System.out.println("\u001B[32mUsuário cadastrado com sucesso!\u001B[0m");
+    }
+
 }
