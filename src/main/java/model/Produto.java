61b2c82c6f2c8ebd3498752f5c81d7a69dd8fca1
Lucas Eduardo <lucaszarzabarros@gmail.com>
2026-03-08 22:04:32 -0300

    Criado a Classe Produto  para ser utilizado na Main para cadastro e atualização do Produto

--- src/main/java/model/Produto.java
+++ src/main/java/model/Produto.java
@@ -1,7 +1,14 @@
 package model;
 
-public class Produto {
-    public static void main(String[] args) {
-        System.out.println("Teste");
+// Herda de Item, representa um produto físico da oficina
+public class Produto extends Item {
+    // Atributos exclusivos de Produto
+    private int quantidadeEstoque;
+    private String categoria;
+
+    // Construtor, super() preenche id, nome e preco na classe pai
+    public Produto(int id, String nome, double preco, int quantidadeEstoque, String categoria) {
+        super(id, nome, preco);
+        this.quantidadeEstoque = quantidadeEstoque;
+        this.categoria = categoria;
     }
-}
