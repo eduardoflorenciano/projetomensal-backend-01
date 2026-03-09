a7074c0bea879d0add03752560c004f696341099
vittor <vittor.moreno@escola.pr.gov.br>
2026-03-08 22:35:57 -0300

    Listar, exibe todos os produtos cadastrados

--- src/main/java/service/ProdutoService.java
+++ src/main/java/service/ProdutoService.java
@@ -19,4 +19,16 @@ public class ProdutoService {
         proximoId++;
         System.out.println("\u001B[32mProduto cadastrado com sucesso! ID:\u001B[0m " + novoProduto.getId());
     }
+
+    // Listar, exibe todos os produtos cadastrados
+    public void listarTodos() {
+        if (produtos.isEmpty()) {
+            System.out.println("\u001B[31mNenhum produto cadastrado\u001B[0m");
+            return;
+        }
+        System.out.println("\n\033[34m======\033[0m LISTA DE PRODUTOS \033[34m======\033[0m");
+        for (Produto p : produtos) {
+            System.out.println(p.exibirDetalhes());
+        }
+    }
 }
